561ca0164af5082a1895a2813fb42e414addbe72
muthukumaran908 <117386055+muthukumaran908@users.noreply.github.com>
2022-11-04 01:56:46 +0530

    Update mongodb.js

--- mail_sent/mongodb.js
+++ mail_sent/mongodb.js
@@ -1,8 +1,8 @@
  module.exports = {
  
-    DB:"mongodb+srv://dev_user:Amsdevuser@cluster0-m5tfq.mongodb.net/rdpmsdev?retryWrites=true"
+    DB:"mongodb+srv://dev_user:Smsdevuser@cluster0-m5tfq.mongodb.net/rddev?retryWrites=true"
   
-    // DB:"mongodb+srv://santhosh:santhosh@cluster0-m5tfq.mongodb.net/rdpms?retryWrites=true"
+
   
   
   }
