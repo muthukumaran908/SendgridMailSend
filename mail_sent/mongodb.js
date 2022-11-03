0852ea12a63e5efa6ab2fdaf34dea142427cbb8a
muthukumaran908 <117386055+muthukumaran908@users.noreply.github.com>
2022-11-03 23:37:56 +0530

    Create mongodb.js

---
+++ mail_sent/mongodb.js
@@ -0,0 +1,12 @@
+ module.exports = {
+ 
+    DB:"mongodb+srv://dev_user:Amsdevuser@cluster0-m5tfq.mongodb.net/rdpmsdev?retryWrites=true"
+  
+    // DB:"mongodb+srv://santhosh:santhosh@cluster0-m5tfq.mongodb.net/rdpms?retryWrites=true"
+  
+  
+  }
+
+
+
+  
